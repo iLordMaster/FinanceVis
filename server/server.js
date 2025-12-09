570c019d4e143289dcd39a0e40fc1fcbd15e25a2
iLordMaster <ayoubthki@gmail.com>
2025-12-09 23:39:41 +0100

    update

--- server/server.js
+++ server/server.js
@@ -66,5 +66,9 @@ app.get("/", (req, res) => {
   res.send("Server running");
 });
 
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Not Found" });
+});
+
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
